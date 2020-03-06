56aa361488c11d1014365e4c78fc2081ee9359f8
Evgeniy Kondratyev <kondratiev.evg@gmail.com>
2020-03-06 16:07:21 +0300

    added some code

--- RPG-battle/index.js
+++ RPG-battle/index.js
@@ -1,8 +1,35 @@
-const {moster,estafiy}=require('./heroes');
-
+const readlineSync = require('readline-sync');
+const {monster,estafiy}=require('./heroes');
+let randomInt=(n)=>{
+    return Math.floor(Math.random()*n)
+}
 let battle={
-    moves:0,
-    
+    moves:0,   
 }
 
-console.log(estafiy)
+monster.freezedMoves=[];
+estafiy.freezedMoves=[]
+
+let monsterMove=()=>{
+    let r=randomInt(monster.moves.length)
+    
+    if(monster.moves[r].cooldown>0){
+        monster.freezedMoves.push(monster.moves[r])
+    }
+    console.log(`Мостр делает ${monster.moves[r].name}`)
+}
+let estafiyMove=()=>{
+    let r=randomInt(estafiy.moves.length)
+
+    if(monster.moves[r].cooldown>0){
+        freezedMoves.push(monster.moves[r])
+    }
+    console.log(`Мостр делает ${monster.moves[r].name}`)
+}
+const start=()=>{
+    monsterMove()
+    let userName = readlineSync.question('? ');
+
+}
+
+start();
