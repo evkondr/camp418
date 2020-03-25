19e46bd887a7f779c998340a7fcbb4f802b11fc9
Evgeniy Kondratyev <kondratiev.evg@gmail.com>
2020-03-25 16:49:53 +0300

    added server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/getfield',(req,res)=>{
+    res.send('ok',)
+})
+app.post('/move',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
