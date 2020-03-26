68333d98fc0ec8035615008c2a359ae6021ce062
evkondr <kondratiev.evg@gmail.com>
2020-03-27 01:09:48 +0300

    added logger

--- server/index.js
+++ server/index.js
@@ -2,16 +2,13 @@
 const express = require('express');
 
 const app = express();
+const routes = require('./src/routes');
+
+app.use(routes);
+
 const port = 3000;
 app.use(express.json());
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
-
-app.get('/getfield', (req, res) => {
-    res.send('ok');
-});
-app.post('/move', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const logger = require('./lib/logger');
 
+let arr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 router.get('/getfield', (req, res) => {
     res.send('ok');
 });
 router.post('/move', (req, res) => {
-    console.log(req.body);
+    logger.log(req.body);
     res.send(req.body);
 });
+module.exports = router;
